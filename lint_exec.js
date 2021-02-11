d07fd4c1d067a5658f6abfda8c8fcf0acf5c264a
YutamaKotaro <california.dreamin.never.die@gmail.com>
2021-02-11 19:02:21 +0900

    ファイル変更時も対象としてlintをかけるようにした。

--- lint_exec.js
+++ lint_exec.js
@@ -1,8 +1,13 @@
-const { execSync, spawn,  } = require("child_process");
+const {execSync, spawn} = require('child_process');
 
+const list =
+  execSync('git diff --name-only') + execSync('git diff --name-only --cached');
+const lintList = list
+  .toString()
+  .split('\n')
+  .filter(fileName => /\.md$/.test(fileName));
 
-const list = execSync('git diff --name-only') + execSync('git diff --name-only --cached')
-const lintList = list.toString().split('\n').filter(fileName => /\.md$/.test(fileName))
-
-
-spawn('npm', ['run', '_text_lint', lintList.join(' ')], {stdio: "inherit", shell: true })
+spawn('npm', ['run', '_text_lint', lintList.join(' ')], {
+  stdio: 'inherit',
+  shell: true,
+});
